ee9a763eea9daa3aea4cd9808341f19b64cf3813
Ofir Shapira <ofirs2003@gmail.com>
2020-09-14 14:55:51 +0300

    fix all bugs

---
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.choose_pic);
+        Button button1 = findViewById(R.id.open_camera);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChoosePicActivity.class);
+                startActivity(intent);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Phone-Detector
-Android application for detecting phone numbers and dealing with this.
-
-## About
-The program is written by a super religious guy and by Nitay Kessner.
