107a9412b89a4c67d4b58c9b8cfe40104c3ce114
Ofir Shapira <ofirs2003@gmail.com>
2020-09-14 15:06:13 +0300

    add camera and storage permission

--- Application/app/src/main/java/com/example/application/MainActivity.java
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.ActionMode;
 import android.view.View;
@@ -28,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
