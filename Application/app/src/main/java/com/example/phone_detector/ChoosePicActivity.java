59dd47c91cb60936bd90835766d411c4bc89d052
Nitay Kessner <nitayke@gmail.com>
2020-09-16 12:51:16 +0300

    trying to use google OCR

--- Application/app/src/main/java/com/example/phone_detector/ChoosePicActivity.java
+++ Application/app/src/main/java/com/example/phone_detector/ChoosePicActivity.java
@@ -7,16 +7,12 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 
-import java.io.IOException;
-<<<<<<< HEAD
-=======
-import java.io.OutputStream;
+import com.google.android.gms.vision.text.TextRecognizer;
 
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
->>>>>>> d7cf6bb181a8c2db21a7bcda47a2a80bcaf1cce7
+import java.io.IOException;
 
 
 public class ChoosePicActivity extends AppCompatActivity
@@ -51,21 +47,9 @@ public class ChoosePicActivity extends AppCompatActivity
                 }
                 ImageView imageView = findViewById(R.id.img);
                 imageView.setImageBitmap(bmp);
-
-<<<<<<< HEAD
-
-=======
-                Tesseract tesseract = new Tesseract();
-                tesseract.setDatapath("C:/Users/Administrator/Downloads/Tess4J/tessdata");
-
-                /* this line is wrong
-                try {
-                    String text = tesseract.doOCR(new File("C:\\Users\\SharpComp\\Pictures\\avatar.png"));
-                } catch (TesseractException e) {
-                    e.printStackTrace();
-                }
-                */
->>>>>>> d7cf6bb181a8c2db21a7bcda47a2a80bcaf1cce7
+                TextRecognizer textRecognizer = new TextRecognizer().Builder(getApplicationContext());
+                if (!textRecognizer.isOperational())
+                    Log.w("MainActivity", "Detector dependencies are not yet available");
             }
         }
 
